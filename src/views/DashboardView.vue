bf926a46f3c1097e100d236f600152611e974daa
hannanmiah <hannanhridoy@gmail.com>
2022-05-30 18:19:26 +0600

    fix: typo

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth";
+
+const auth = useAuthStore();
+</script>
 
 <template>
   <div class="flex flex-col space-y-2 px-10 py-4 md:space-y-4">
-    <h1 class="text-3xl">Hi, Hannan Miah. Welcome to your dashboard</h1>
+    <h1 class="text-3xl">
+      Hi, {{ auth.user.name }}. Welcome to your dashboard
+    </h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
       doloribus ratione incidunt a similique aperiam repellendus rem architecto
